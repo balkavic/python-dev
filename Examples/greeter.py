6fb6f86c64992912a2e7e855df20a58d4dd50b2d
Balazs Nyevrikel <nybazsi@gmail.com>
2021-01-07 15:00:16 +0100

    minor

--- Examples/greeter.py
+++ Examples/greeter.py
@@ -3,4 +3,13 @@ def greet_user(username):
     print(f"Hello, {username.title()}!")
 
 
-greet_user("jim")
+# greet_user("jim")
+
+def make_shirt(size, text):
+    print(f"Making shirt of size {size} with text '{text}'")
+
+# make_shirt("L", "This is my shirt")
+# make_shirt(text="Second shirt", size="XL")
+
+
+

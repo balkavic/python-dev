d4745fd87dc93707a19b102c4372329ec4cece44
Balazs Nyevrikel <nybazsi@gmail.com>
2021-02-07 19:13:31 +0100

    More examples

--- Examples/file_reader.py
+++ Examples/file_reader.py
@@ -6,21 +6,42 @@ with open(filename) as file_object:
     #     print(line.rstrip())
     lines = file_object.readlines()
 
-pi_string = ''
+# pi_string = ''
+#
+# for line in lines:
+#     pi_string += line.strip()
+#
+# print(f"{pi_string[:102]}...")
+# print(len(pi_string))
 
-for line in lines:
-    # print(line.rstrip())
-    pi_string += line.strip()
-
-print(f"{pi_string[:102]}...")
-print(len(pi_string))
-
-birthday = input("Enter your birthday, in the form mmddyy: ")
-
-if birthday in pi_string:
-    print("Your birthday appears in the first million digits of pi!")
-    print(pi_string.find(birthday))
-else:
-    print("Your birthday does not appear in the first million digits of pi.")
+# birthday = input("Enter your birthday, in the form mmddyy: ")
+#
+# if birthday in pi_string:
+#     print("Your birthday appears in the first million digits of pi!")
+#     print(pi_string.find(birthday))
+# else:
+#     print("Your birthday does not appear in the first million digits of pi.")
 
 # print(contents.rstrip())
+
+filename = 'learning_python.txt'
+
+with open(filename) as file_object:
+    contents = file_object.read()
+
+print(contents)
+print()
+
+with open(filename) as file_object:
+    for line in file_object:
+        print(line.rstrip())
+
+lines = []
+
+with open(filename) as file_object:
+    for line in file_object:
+        lines.append(line.rstrip().replace('Python', 'C'))
+
+for line in lines:
+    print(line)
+

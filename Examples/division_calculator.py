b5a0619da3e4f888b1038ced84adb8bbfe0f050d
Balazs Nyevrikel <nybazsi@gmail.com>
2021-09-30 17:03:07 +0200

    Added more Python examples

--- Examples/division_calculator.py
+++ Examples/division_calculator.py
@@ -1,1 +1,27 @@
-print(5/0)
+def divide(first, second):
+    try:
+        answer = int(first_number) / int(second_number)
+    except ZeroDivisionError:
+        print("You cannot divide by zero!")
+    else:
+        print(answer)
+
+try:
+    print(5/0)
+except ZeroDivisionError:
+    print("You cannot divide by zero!")
+
+print("Give me two numbers, and I'll divide them.")
+print("Enter 'q' to quit.")
+
+while True:
+    first_number = input("\nFirst number:")
+    if first_number == 'q':
+        break
+    second_number = input("\nSecond number:")
+    if second_number == 'q':
+        break
+    divide(first_number, second_number)
+
+
+
